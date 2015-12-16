fad120584fdd4dc111c169c59a95a5aa0f76b178
Sean Holcomb <seanholcomb@gmail.com>
2015-12-15 17:40:23 -0800

    feat: flow fully functional

--- application.py
+++ application.py
@@ -151,7 +151,7 @@ def gdisconnect():
 @app.route("/catalog/add", methods = ['GET', 'POST'])
 def addItem():
 	if request.method == 'POST':
-		#probably has problem with drop down selector
+		print request.form['catagory']
 		catagory = session.query(Catagory).filter_by(name = request.form['catagory']).one()
 		newItem = Item(title = request.form['name'], description = request.form['description'], catagory = catagory)
 		session.add(newItem)
@@ -161,20 +161,17 @@ def addItem():
 		catagories = session.query(Catagory).all()
 		return render_template('new.html', catagories=catagories)
 
-#conflict between url and varible
 @app.route("/catalog/<int:catagory_id>")
 def getItems(catagory_id):
 	catagories = session.query(Catagory).all()
 	items = session.query(Item).filter_by(catagory_id = catagory_id).all()
 	return render_template('main.html', catagories=catagories, items=items)
 
-#conflict between url and varible
 @app.route("/catalog/<int:catagory_id>/<int:item_id>/")
 def getItem(catagory_id, item_id):
 	item = session.query(Item).filter_by(id = item_id).one()
 	return render_template('item.html', item = item)
 
-#conflict between url and varible
 @app.route("/catalog/<int:catagory_id>/<int:item_id>/edit", methods = ['GET', 'POST'])
 def editItem(catagory_id, item_id):
 	editItem = session.query(Item).filter_by(id = item_id).one()
@@ -184,9 +181,10 @@ def editItem(catagory_id, item_id):
 			editItem.title = request.form['name']
 		if request.form['description']:
 			editItem.description = request.form['description']
-		#if request.form['catagory']:
-		#	catagory = session.query(Catagory).filter_by(name = request.form['catagory']).one()
-		#	editItem.catagory = catagory
+
+		if request.form['catagory']:
+		  catagory = session.query(Catagory).filter_by(name = request.form['catagory']).one()
+		  editItem.catagory = catagory
 		session.add(editItem)
 		session.commit()
 		return redirect(url_for('getItems', catagory_id = editItem.catagory_id))
